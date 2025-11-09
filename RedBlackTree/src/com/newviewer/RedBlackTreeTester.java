e25211572db6d637d30fe412d039c647a1243bd8
catmw <157846537+catmw@users.noreply.github.com>
2025-11-09 16:57:57 +0000

    UpdateTester

--- RedBlackTree/src/com/newviewer/RedBlackTreeTester.java
+++ RedBlackTree/src/com/newviewer/RedBlackTreeTester.java
@@ -13,12 +13,33 @@ public class RedBlackTreeTester {
 	    myTree.insert(2);
 	    myTree.insert(3);
 
-
-
-	
-		System.out.println("Pre-order Traversal:");
+		//Test1
+		System.out.println("Pre-order Traversal after inserting 1, 2, 3:");
 		myTree.preOrderTraversal();
 		System.out.println();
+
+
+		myTree.insert(12);
+	    myTree.insert(7);
+	    myTree.insert(4);
+		myTree.insert(5);
+
+		//Test2 
+		System.out.println("In-order Traversal after inserting 1, 2, 3, 12, 7, 4, 5:");
+		myTree.preOrderTraversal();
+		System.out.println();
+
+		//Test3
+		System.out.println("Node count should be 7 == " + myTree.countNodes());
+
+		//Test4
+        System.out.println("Min should be 1 == " + myTree.findMinimum());
+
+		//Test5
+        System.out.println("Max should be 12 == " + myTree.findMaximum());
+
+		//Test6
+		System.out.println("root: " + myTree.root.value);
 	}
 
 }
